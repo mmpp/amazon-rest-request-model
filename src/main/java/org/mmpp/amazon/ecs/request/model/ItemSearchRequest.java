88b1604f678330aeb40964c5ce14c6cdbefa0ae3
wataru <kou_wataru@hotmail.com>
2012-07-23 10:38:40 +0900

    ItemSearchRequestの作成

---
+++ src/main/java/org/mmpp/amazon/ecs/request/model/ItemSearchRequest.java
@@ -0,0 +1,32 @@
+package org.mmpp.amazon.ecs.request.model;
+
+public class ItemSearchRequest extends AbstractRequest{
+	private SearchIndex _searchIndex;
+	private java.util.Map<SearchType,String> _searchParameters;
+	/**
+	 * 検索結果から10Itemを1ページとしてページングした
+	 * 何ページ目のデータを取得するかを指示する
+	 * default null : 1ページ目
+	 */
+	private Integer _itemPage = null;;
+	
+	public ItemSearchRequest(){
+		super();
+		_searchParameters = new java.util.HashMap<SearchType,String>();
+	}
+	public void setSearchIndex(SearchIndex searchIndex) {
+		_searchIndex = searchIndex;
+	}
+	public SearchIndex getSearchIndex(){
+		return _searchIndex;
+	}
+	public java.util.Map<SearchType,String> getSearchParameters(){
+		return _searchParameters;
+	}
+	public Integer getItemPage() {
+		return _itemPage;
+	}
+	public void setItemPage(int itemPage){
+		_itemPage = itemPage;
+	}
+}
---
+++ src/main/java/org/mmpp/amazon/ecs/request/model/SearchType.java
@@ -0,0 +1,24 @@
+package org.mmpp.amazon.ecs.request.model;
+
+public enum SearchType {
+	Keywords,
+	Title,
+	Power,
+	BrowseNode,
+	Artist,
+	Author,
+	Actor,
+	Director,
+	AudienceRating,
+	Manufacturer,
+	MusicLabel,
+	Composer,
+	Publisher,
+	Brand,
+	Conductor,
+	Orchestra,
+	TextStream,
+	Cuisine,
+	City,
+	Neighborhood,
+}
